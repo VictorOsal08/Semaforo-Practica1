90f68b83928fa9350f8547f995b55b2dda98c554
VictorOsal08 <Victorrodrguez706@gmail.com>
2026-03-06 09:38:57 -0400

    configuración de pines

---
+++ grupo_31.ino
@@ -0,0 +1,19 @@
+void setup()
+{
+pinMode(13, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+}
